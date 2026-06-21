575a8d1f7a875cabd03b66d58b817968e37b3307
Earth1283 <142399154+Earth1283@users.noreply.github.com>
2026-06-21 18:39:24 +0800

    Initial commit

---
+++ src/main/kotlin/io/github/Earth1283/teletype/Teletype.kt
@@ -0,0 +1,14 @@
+package io.github.Earth1283.teletype
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Teletype : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
